6fa021b3971e150b5748c9fb39b95a32136f759f
2475663 <Tamer.zara2015@gmail.com>
2023-08-10 17:01:39 -0700

    3-name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@ n
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzl                                       e");
+puts("\"Programming is like building a multilingual puzz                                        le");
 return (0);
 }
